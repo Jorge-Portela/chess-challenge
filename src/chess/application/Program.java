4b518045714b07d4e31db131e84e005281ece720
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-09 22:51:00 -0300

    Added 'Position' Class

--- src/chess/application/Program.java
+++ src/chess/application/Program.java
@@ -1,4 +1,21 @@
 package chess.application;
 
+import chess.boardGame.Position;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
+
+        Position ps1 = new Position(reader.nextInt(), reader.nextInt());
+
+        System.out.println(ps1);
+
+
+        reader.close();
+    }
 }
